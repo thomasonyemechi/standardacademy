8e2da137642a2090fce11f8038bae9413e62ec3a
Thomas Onyemechi <66361999+thomasonyemechi@users.noreply.github.com>
2023-08-17 23:05:00 +0100

    res

--- app/Http/Controllers/ResultController.php
+++ app/Http/Controllers/ResultController.php
@@ -9,6 +9,7 @@ class ResultController extends Controller
 {
     function uploadResultIndex()
     {
-        $class = ClassCore::where('class_teacher', auth()->user()->id)->first
+        $class = ClassCore::where('class_teacher', auth()->user()->id)->first();
+        
     }
 }
